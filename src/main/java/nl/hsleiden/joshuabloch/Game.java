8375936ae686908b36fc57a8c933cb327cece296
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-04-06 13:52:30 +0200

    Voeg boilerplate toe

--- src/main/java/nl/hsleiden/joshuabloch/Game.java
+++ src/main/java/nl/hsleiden/joshuabloch/Game.java
@@ -1,7 +1,45 @@
 package nl.hsleiden.joshuabloch;
 
-public class Game {
-    public static void main(String[] args) {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 
+import java.util.Map;
+
+public class Game extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(600);
+        settings.setHeight(600);
+        settings.setTitle("PC builder : ");
+        settings.setVersion("2021");
+    }
+
+    @Override
+    protected void initInput() {
+
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+    }
+
+
+    @Override
+    protected void initGame() {
+
+    }
+
+    @Override
+    protected void initPhysics() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
