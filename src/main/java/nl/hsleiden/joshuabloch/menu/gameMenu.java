9ed82911eafd1e576d1cc3b7066fa19180a2d1be
s1124723 <73645221+s1124723@users.noreply.github.com>
2021-04-08 14:48:50 +0200

    GameMenu text center, add image.

--- src/main/java/nl/hsleiden/joshuabloch/menu/gameMenu.java
+++ src/main/java/nl/hsleiden/joshuabloch/menu/gameMenu.java
@@ -21,6 +21,8 @@ import javafx.scene.shape.Shape;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.awt.*;
 import java.security.cert.PolicyNode;
@@ -36,6 +38,21 @@ public class gameMenu extends FXGLMenu {
         getContentRoot().setTranslateX(FXGL.getAppWidth() / 2.0 - SIZE);
         getContentRoot().setTranslateY(FXGL.getAppHeight() / 2.0 - SIZE);
 
+        ImageView imageView = new ImageView(new Image("assets/textures/forest.png"));
+
+        imageView.setFitWidth(FXGL.getAppWidth());
+        imageView.setFitHeight(FXGL.getAppHeight());
+
+//        imageView.setTranslateX();
+//        imageView.setTranslateY();
+
+        // Button Resume
+        var buttonResume = new Rectangle(SIZE*2, SIZE / 2);
+        buttonResume.setStrokeWidth(2.5);
+        buttonResume.strokeProperty().bind(Bindings.when(buttonResume.hoverProperty()).then(Color.YELLOW).otherwise(Color.BLACK));
+        buttonResume.fillProperty().bind(Bindings.when(buttonResume.pressedProperty()).then(Color.YELLOW).otherwise(Color.color(0.1, 0.05, 0.0, 0.75)));
+        buttonResume.setTranslateY(-50);
+        buttonResume.setOnMouseClicked(e -> FXGL.getGameController().gotoPlay());
 
         // Button Play
         var buttonPlay = new Rectangle(SIZE*2, SIZE / 2);
@@ -61,46 +78,52 @@ public class gameMenu extends FXGLMenu {
         buttonQuit.setTranslateY(250);
         buttonQuit.setOnMouseClicked(e -> FXGL.getGameController().exit());
 
-
+        // Text Play
+        Text textResume = FXGL.getUIFactoryService().newText("RESUME", Color.RED, FontType.GAME, 24.0);
+        textResume.setTextAlignment(TextAlignment.CENTER);
+        textResume.setWrappingWidth(buttonResume.getWidth());
+        textResume.setTranslateY(buttonResume.getTranslateY() + (buttonResume.getHeight() / 2) + (textResume.getLayoutBounds().getHeight() / 4) );
 
         // Text Play
-        Text textPlay = FXGL.getUIFactoryService().newText("RESTART", Color.RED, FontType.GAME, 24.0);
+        Text textPlay = FXGL.getUIFactoryService().newText("RESTART LEVEL", Color.RED, FontType.GAME, 24.0);
         textPlay.setTextAlignment(TextAlignment.CENTER);
         textPlay.setWrappingWidth(buttonPlay.getWidth());
         textPlay.setTranslateY(buttonPlay.getTranslateY() + (buttonPlay.getHeight() / 2) + (textPlay.getLayoutBounds().getHeight() / 4) );
 
         // Text mainMenu
         Text textMainMenu = FXGL.getUIFactoryService().newText("BACK TO MENU", Color.RED, FontType.GAME, 24.0);
-        textMainMenu.setTranslateX((buttonMainMenu.getWidth() / 2) -  (textMainMenu.getLayoutBounds().getWidth() / 2));
+        textMainMenu.setTextAlignment(TextAlignment.CENTER);
+        textMainMenu.setWrappingWidth(buttonMainMenu.getWidth());
         textMainMenu.setTranslateY(buttonMainMenu.getTranslateY() + (buttonMainMenu.getHeight() / 2) + (textMainMenu.getLayoutBounds().getHeight() / 4) );
 
         // Text Quit
-        Text textQuit = FXGL.getUIFactoryService().newText(" QUIT", Color.RED, FontType.GAME, 24.0);
-        textQuit.setTranslateX((buttonQuit.getWidth() / 2) - (textQuit.getLayoutBounds().getWidth() / 2));
+        Text textQuit = FXGL.getUIFactoryService().newText("QUIT", Color.RED, FontType.GAME, 24.0);
+        textQuit.setTextAlignment(TextAlignment.CENTER);
+        textQuit.setWrappingWidth(buttonQuit.getWidth());
         textQuit.setTranslateY(buttonQuit.getTranslateY() + (buttonQuit.getHeight() / 2) + (textQuit.getLayoutBounds().getHeight() / 4) );
 
-
-
-//        System.out.println(buttonPlay.getTranslateY());
-//        System.out.println(buttonPlay.getTranslateY() + (SIZE / 2));
+        textResume.setMouseTransparent(true);
         textPlay.setMouseTransparent(true);
+        textMainMenu.setMouseTransparent(true);
         textQuit.setMouseTransparent(true);
 
 
 
-
-        getContentRoot().getChildren().addAll(buttonPlay, textPlay, buttonQuit, textQuit, buttonMainMenu, textMainMenu);
-
+        FXGL.getWindowService().getOverlayRoot().getChildren().addAll(imageView);
+        getContentRoot().getChildren().addAll(buttonPlay, textPlay, buttonQuit, textQuit, buttonMainMenu, textMainMenu, buttonResume, textResume);
+        FXGL.getWindowService().getOverlayRoot().setScaleX(0);
+        FXGL.getWindowService().getOverlayRoot().setScaleY(0);
         getContentRoot().setScaleX(0);
         getContentRoot().setScaleY(0);
 
         animation = FXGL.animationBuilder()
 
-                .duration(Duration.seconds(0.66))
+                .duration(Duration.seconds(1.66))
 
                 .interpolator(Interpolators.EXPONENTIAL.EASE_OUT())
 
                 .scale(getContentRoot())
+                .scale(FXGL.getWindowService().getOverlayRoot())
 
                 .from(new Point2D(0, 0))
 
